40f80c10d578d42a7eea489a1f898f9b692d130b
alvaro <alvaroabascar@gmail.com>
2015-12-12 02:39:23 +0100

    support functions and set!

--- lis.py
+++ lis.py
@@ -1,9 +1,102 @@
+from numbers import Number
+import math
+import operator as op
+
+# a symbol is anything that can hold a value. We represent it as a string.
+Symbol = str
+
+class Procedure:
+    """
+    A procedure is just a function. It is composed by a set of parameters,
+    a body (which will be evaluated) and an environment.
+    """
+    def __init__(self, args, body, env):
+        self.args = args
+        self.body = body
+        self.env = env
+
+    def __call__(self, *values):
+        local_env = Env(upper_env=self.env)
+        local_env.update(zip(self.args, values))
+        return eval(self.body, local_env)
+
+class Env(dict):
+    """
+    A lispy environment. It is instantiated with a set of symbols (params),
+    the corresponding values (values), and an upper environment (upper_env),
+    which is the parent environment (or None if this is the uppermost
+    environment).
+
+    """
+    def __init__(self, params=[], values=[], upper_env=None):
+        self.update(zip(params, values))
+        self.upper = upper_env
+
+    def find(self, param):
+        """
+        Returns the lowermost environment which contains the provided symbol
+        (param), starting from the current environment and tracing upwards.
+        """
+        return self if param in self else self.upper.find(param)
+
+def eval(x, env):
+    """Evaluate an expression (x) given a certain environment (env)."""
+
+    if isinstance(x, Number):
+        return x
+
+    # is it a variable?
+    elif isinstance(x, Symbol):
+        return env.find(x)[x]
+
+    # is it a definition?
+    # (define name value)
+    elif x[0] == "define": # (define variable value)
+        _, var, expr = x
+        env[var] = eval(expr, env)
+
+    # is it a conditional?
+    # (if (condition) (consequence) (alternative))
+    elif x[0] == "if":
+        _, cond, conseq, alt = x
+        if eval(cond, env):
+            return eval(conseq, env)
+        else:
+            return eval(alt, env)
+
+    # is it a lambda function?
+    # (lambda (args...) (body))
+    elif x[0] == "lambda":
+        _, args, body = x
+        return Procedure(args, body, env)
+
+    # is it a direct quotation? return argument(s) without evaluating
+    # (quote a b c)
+    elif x[0] == "quote":
+        return x[1:]
+
+    # is it a set? (definition of already existing variable)
+    elif x[0] == "set!":
+        _, var, expr = x
+        env.find(var)[var] = eval(expr, env)
+    # it must be a function
+    else:
+        proc, args = eval(x[0], env), x[1:]
+        args = [eval(arg, env) for arg in args]
+        return proc(*args)
+
 def tokenize(program):
     """Split the program into tokens (words, ")" and "(")."""
     return program.replace("(", " ( ").replace(")", " ) ").split()
 
 def atom(token):
-    return token
+    try:
+        return int(token)
+    except ValueError:
+        try:
+            return float(token)
+        except ValueError:
+            return Symbol(token)
 
 def read_from_tokens(tokens):
     """
@@ -30,6 +123,33 @@ def parse(program):
     """Go from the raw program code to the abstract syntax tree."""
     return read_from_tokens(tokenize(program))
 
+def standard_env():
+    env = Env()
+    env.update(vars(math))
+    env.update({
+        '*': op.mul,
+        '/': op.truediv,
+        '+': op.add,
+        '-': op.sub,
+        '>=': op.ge,
+        '<=': op.le,
+        '>': op.gt,
+        '<': op.lt,
+        '=': op.eq
+        })
+    return env
+
+def repl(env, prompt='lispy> '):
+    expr = input(prompt)
+    res = eval(parse(expr), env)
+    if res != None:
+        print(res)
+    repl(env, prompt)
+
 if __name__ == '__main__':
     import sys
+    env = standard_env()
+    if len(sys.argv) <= 1:
+        repl(env)
+    print(eval(parse(sys.argv[1]), env))
     print(parse(sys.argv[1]))
